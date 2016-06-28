618b83eba0efdc103b96c78be97d2debbbe86e99
Miles Prower <tails@milesprower.com>
2016-06-28 04:28:29 -0400

    Auto deployment test

--- cl_qac.lua
+++ cl_qac.lua
@@ -1,9 +1,10 @@
 ---- CL_QAC
----- CH 2/2/14, original copy @ 9/13
+---- CH 2/2/14, original copy @ 9/134
 ---- ZTF 
 ---- I don't care if you learn how to decrypt the lua cache and steal this, so stopping adding me about it
 ---- Tho pref dont redistribute ty
-local nr = _G["net"]["Receive"]
+
+local nr = _G["net"]["Receive"]
 local ns = _G["net"]["Start"]
 local ns2s = _G["net"]["SendToServer"]
 local nws = _G["net"]["WriteString"]
@@ -15,32 +16,50 @@ local cvarcb = cvars.AddChangeCallback
 local dbgg = debug.getinfo
 local up = unpack
 local gc = GetConVar
-
---Source Detection ThingsQAC = true
 
-local scans = {}
-local scanf = {
-	{hook, "Add"},
-	--{hook, "Call"}, -- cl deathnotice is retarded?  // needs to be fixed. -- esp
-	--{hook, "Run"}, -- cl deathnotice is retarded?
-	{timer, "Create"},
-	{timer, "Simple"},
-	--{_G, "CreateClientConVar"}, -- ULX IS GAY   // note to self, create detour
-	--{_G, "RunString"}, -- cl deathnotice is retarded?
+
+--Source Detection Things
+QAC = true
+
+local scans = {}
+
+local scanf = {
+
+	{hook, "Add"},
+
+	--{hook, "Call"}, -- cl deathnotice is retarded?  // needs to be fixed. -- esp
+
+	--{hook, "Run"}, -- cl deathnotice is retarded?
+
+	{timer, "Create"},
+
+	{timer, "Simple"},
+
+	--{_G, "CreateClientConVar"}, -- ULX IS GAY   // note to self, create detour
+
+	--{_G, "RunString"}, -- cl deathnotice is retarded?
+
 	//{_G, "RunStringEx"},
 	
 	//{file, "Read"},
 	
-	{concommand, "Add"},
+	{concommand, "Add"},
+
 	{_G, "RunConsoleCommand"}
 
-}
+}
 
-local function validate_src(src)
-	ns("checksaum")
-		nws(src)
-	ns2s()
-end
+
+local function validate_src(src)
+
+	ns("checksaum")
+
+		nws(src)
+
+	ns2s()
+
+end
+
 local function RFS()
 
 	local CNum = net.ReadInt(10)
@@ -56,106 +75,205 @@ nr("Debug2", RFS)
 
 nr("QUACK_QUACK_MOTHER_FUCKER", function()
 	LocalPlayer():EmitSound("qac/quack.wav") -- RIP NIGGERS
-end)
-local function scan_func()
-	local s = {}
-	
-	for i = 0, 1/0, 1 do
+end)
+
+local function scan_func()
+
+	local s = {}
+
+	
+
+	for i = 0, 1/0, 1 do
+
 	local dbg = dbgg(i)
 		
-		if (dbg) then
-			s[dbg.short_src] = true
-		else
-			break
-		end
-	end
-	
-	for src, _ in pairs(s) do
-		if (src == "RunString" || src == "LuaCmd" || src == "[C]") then
-			return
-		elseif (!(scans[src])) then
-			scans[src] = true
-			validate_src(src)
-		end
-	end
-end
-
----Scan Functions
-local function SCAN_G()
-	for _, ft in pairs(scanf) do
-		local ofunc = ft[1][ft[2]]
-		
-		ft[1][ft[2]] = (
-			function(...)
-				local args = {...}
-				scan_func()
-				ofunc(up(args))
-			end
-		)
-	end
-end
-
-hook.Add(
-	"OnGamemodeLoaded",
-	"___scan_g_init",
-	function()
-		SCAN_G()
-		hook.Remove("OnGamemodeLoaded", "___scan_g_init")
-	end
-)
-
-
---ConVar Detection
-local function validate_cvar(c, v)
-	ns("control_vars")
-		nwt({c = c, v = v})
-	ns2s()
-end
-
-
-local function cvcc(cv, pval, nval)
-	validate_cvar(cv, nval)
-end
-
-local ctd = {}
-
-local function sned_req()
-	ns("gcontrol_vars")
-		nwb()
-	ns2s()
-end
-_G.timer.Simple(1, sned_req)
-
-
-nr(
-	"gcontrol_vars",
-	function()
-		local t = nrt()
-		
-		local c = gc(t.c)
-		local v = (c != nil && c:GetString() || "0")
-		ctd[c] = v
-		cvarcb(t.c, cvcc)
-		if (v != t.v) then
-			validate_cvar(t.c, v)
-		end
-	end
-)
-
----Timed Chec
-local mintime = 010
-local maxtime = 030
-
-local function timecheck()
-	for c, v in pairs(ctd) do
-		local cv = c:GetString() || ""
-		if (cv != v) then
-			validate_cvar(c:GetName(), cv)
-			ctd[c] = cv
-		end
-	end
-	timer.Simple(math.random(mintime, maxtime), timecheck)
-end
+		if (dbg) then
+
+			s[dbg.short_src] = true
+
+		else
+
+			break
+
+		end
+
+	end
+
+	
+
+	for src, _ in pairs(s) do
+
+		if (src == "RunString" || src == "LuaCmd" || src == "[C]") then
+
+			return
+
+		elseif (!(scans[src])) then
+
+			scans[src] = true
+
+			validate_src(src)
+
+		end
+
+	end
+
+end
+
+
+
+---Scan Functions
+
+local function SCAN_G()
+
+	for _, ft in pairs(scanf) do
+
+		local ofunc = ft[1][ft[2]]
+
+		
+
+		ft[1][ft[2]] = (
+
+			function(...)
+
+				local args = {...}
+
+				scan_func()
+
+				ofunc(up(args))
+
+			end
+
+		)
+
+	end
+
+end
+
+
+
+hook.Add(
+
+	"OnGamemodeLoaded",
+
+	"___scan_g_init",
+
+	function()
+
+		SCAN_G()
+
+		hook.Remove("OnGamemodeLoaded", "___scan_g_init")
+
+	end
+
+)
+
+
+
+
+
+--ConVar Detection
+
+local function validate_cvar(c, v)
+
+	ns("control_vars")
+
+		nwt({c = c, v = v})
+
+	ns2s()
+
+end
+
+
+
+
+
+local function cvcc(cv, pval, nval)
+
+	validate_cvar(cv, nval)
+
+end
+
+
+
+local ctd = {}
+
+
+
+local function sned_req()
+
+	ns("gcontrol_vars")
+
+		nwb()
+
+	ns2s()
+
+end
+
+_G.timer.Simple(1, sned_req)
+
+
+
+
+
+nr(
+
+	"gcontrol_vars",
+
+	function()
+
+		local t = nrt()
+
+		
+
+		local c = gc(t.c)
+
+		local v = (c != nil && c:GetString() || "0")
+
+		ctd[c] = v
+
+
+		cvarcb(t.c, cvcc)
+
+		if (v != t.v) then
+
+			validate_cvar(t.c, v)
+
+		end
+
+	end
+
+)
+
+
+
+---Timed Chec
+
+local mintime = 010
+
+local maxtime = 030
+
+
+
+local function timecheck()
+
+	for c, v in pairs(ctd) do
+
+		local cv = c:GetString() || ""
+
+		if (cv != v) then
+
+			validate_cvar(c:GetName(), cv)
+
+			ctd[c] = cv
+
+		end
+
+	end
+
+	timer.Simple(math.random(mintime, maxtime), timecheck)
+
+end
+
 
 -- file steal pls REMOVED.
 -- Not allowed. Sorry guys!
